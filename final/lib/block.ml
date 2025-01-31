e914df6c78e34a5299d7b4f9f916c3063ee4f967
Ethan Coskay <ethancoskay@gmail.com>
2025-01-30 23:07:00 -0500

    implemented block.ml

--- final/lib/block.ml
+++ final/lib/block.ml
@@ -1,16 +1,10 @@
-(** Representation of the Block Object within the grid. *)
+type 'a t = {
+  data : 'a;
+  x : int;
+  y : int;
+}
 
-type 'a t
-(** Type t represents the block in the form of a record that contains position *)
-
-val new_block : 'a -> int -> int -> 'a t
-(** [new_block] takes [data] [x] and [y] and returns a new block of type t *)
-
-val get_data : 'a t -> 'a
-(** [get_data] returns the data of the block *)
-
-val get_x : 'a t -> int
-(** [get_x] returns the x position of the block *)
-
-val get_y : 'a t -> int
-(** [get_y] returns the y position of the block *)
+let new_block (data : 'a) (x : int) (y : int) : 'a t = { data; x; y }
+let get_data (block : 'a t) : 'a = block.data
+let get_x (block : 'a t) : int = block.x
+let get_y (block : 'a t) : int = block.y
